c11b74a3dd4d8590965bfd73332f30d892e94c98
cryptochadnft <einotiisala@yahoo.com>
2022-03-15 17:33:45 +0200

    added checking wallet for nfts

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 // METAMASK CONNECTION
 const TIMEOUT = 1000;
-const COLLECTION_NAME = 'CodeCats';
+const COLLECTION_NAME = 'Crypto C';
 let editions = [];
 let dots = 1;
 
@@ -78,15 +78,15 @@ function updateStatusText(isOwner, checking) {
   const statusText = document.querySelector('.owner-status');
   if(checking) {
     if(isOwner) {
-      statusText.innerText = `You do own ${COLLECTION_NAME}!! 😻 Let's see how many${renderDots(dots)}`;
+      statusText.innerText = `Chad confirmed, entry granted ${renderDots(dots)}`;
     } else {
-      statusText.innerText = `Checking to see if you own any ${COLLECTION_NAME} 😻${renderDots(dots)}`;
+      statusText.innerText = `Identifying Chad ${renderDots(dots)}`;
     }
   } else {
     if(isOwner) {
-      statusText.innerText = `You own ${editions.length} ${COLLECTION_NAME}!! 😻`;
+      statusText.innerText = `You posses ${editions.length} Chads`;
     } else {
-      statusText.innerText = `You don't own any ${COLLECTION_NAME} 😿`;
+      statusText.innerText = `Chad unidentified, access denied.`;
     }
   }
   dots = dots === 3 ? 1 : dots + 1;
